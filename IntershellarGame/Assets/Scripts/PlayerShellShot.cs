d5aa216244306aa767c4ecd4dccbb0209d6a9600
Weiyan Zhu <weiyanz@uci.edu>
2018-10-19 20:36:16 -0700

    Create files for shellshot

---
+++ IntershellarGame/Assets/Scripts/PlayerShellShot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShellShot : MonoBehaviour {
+	public GameObject shell;//we need to throw sth
+	public int initialRotation;
+	public float projectForce;	
+	public float dashForce;	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//shoot a shell at opposite direction and dash
+	private void ShootShell()
+	{
+		//project shell
+		GameObject shellObject = GameObject.Instantiate(shell, transform.position, Quaternion.identity);
+	} 
+
+}
