e5f4b35dab00de5787eb0ffbe9fe51b36d5ee900
Weiyan Zhu <weiyanz@uci.edu>
2018-10-20 13:33:13 -0700

    Camera Follow

---
+++ IntershellarGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject target;
+	private float speed = 5;
+	[Header("follow in what range?")]
+	public bool hasRange;
+	public Vector2 leftLower;
+	public Vector2 rightUpper;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Follow();
+	}
+
+	private void Follow()
+	{
+		Vector2 distance = target.transform.position - transform.position;
+		if(!hasRange)
+		{
+			transform.position += new Vector3(distance.x, distance.y, 0);
+		}
+		else
+		{ 
+			//move at x direction
+			if(transform.position.x <= rightUpper.x && transform.position.x >= leftLower.x)
+			{
+				transform.position += new Vector3(distance.x, 0, 0);
+			}
+			//move at y direction
+			if(transform.position.y <= rightUpper.y && transform.position.y >= leftLower.y)
+			{
+				transform.position += new Vector3(0, distance.y, 0);
+			}	
+		}	
+	}
+
+}
