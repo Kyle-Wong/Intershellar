ae3c0a82228c406bed05b875ae03733e5c085582
unknown <jagerton@uci.edu>
2018-10-19 22:12:54 -0700

    mostly changes in the scene, some alterations in player movement script

---
+++ IntershellarGame/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+    float velocity = 0;
+    float crab_speed = 1f;
+    float rotation_speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotating = Input.GetAxis("Rotation") * rotation_speed;
+        if (Input.GetButton("Rotation"))
+        {
+            transform.Rotate(0, 0, rotating);
+        }
+        /*if (Input.GetButtonDown("Push"))
+        {
+            transform.Translate
+        }*/
+	}
+    //Activates when crab collides with detached shell. Places shell on top of stack.
+    void Gain_Shell()
+    {
+
+    }
+}
