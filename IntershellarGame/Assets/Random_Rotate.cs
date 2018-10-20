43627119d551fa6d3b3a99b7d6847b4791d3e196
unknown <jagerton@uci.edu>
2018-10-20 16:57:17 -0700

    random rotation w/ adjustable speeds added to shell prefabs

---
+++ IntershellarGame/Assets/Random_Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_Rotate : MonoBehaviour {
+    public float rotate_speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,0,rotate_speed);
+	}
+}
