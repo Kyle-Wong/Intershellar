107c99fe5157bd753a3a8b1768df9e28874417b2
Kyle Wong <kylewonga@gmail.com>
2018-10-19 22:31:13 -0700

    Partial star implementation

---
+++ IntershellarGame/Assets/StarManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarManager : MonoBehaviour {
+
+    // Use this for initialization
+    public Camera mainCamera;
+    public Transform bigStarPrefab;
+    public Transform midStarPrefab;
+    public Transform smallStarPrefab;
+    public int bigStarCount;
+    public int midStarCount;
+    public int smallStarCount;
+    private float screenWidth;
+    private float screenHeight;
+	void Start () {
+        screenWidth = mainCamera.ViewportToWorldPoint(new Vector2(1, 0)).x - mainCamera.ScreenToWorldPoint(new Vector2(0, 0)).x;
+        screenHeight = mainCamera.ViewportToWorldPoint(new Vector2(0, 1)).y - mainCamera.ScreenToWorldPoint(new Vector2(0, 0)).y;
+        print(screenWidth + ", " + screenHeight);
+        generateStars(bigStarPrefab, bigStarCount);
+        generateStars(midStarPrefab, midStarCount);
+        generateStars(smallStarPrefab, smallStarCount);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector3(mainCamera.transform.position.x,mainCamera.transform.position.y);
+	}
+    private void generateStars(Transform starPrefab,int numStars)
+    {
+
+        Vector3 rngTrans = new Vector3();
+        for(int i = 0; i < numStars; i++)
+        {
+            rngTrans = transform.position + Vector3.right * (Random.Range(-screenWidth/2,screenWidth/2)) + Vector3.up * (Random.Range(-screenHeight/2,screenHeight/2));
+            Transform newStar = Instantiate(starPrefab, rngTrans,Quaternion.identity);
+            newStar.parent = transform;
+            newStar.GetComponent<Star>().setBounds(screenWidth,screenHeight);
+        }
+    }
+}
