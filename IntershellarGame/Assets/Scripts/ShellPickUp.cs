ae20921214537cebc06e84016918b9a1e1f5e069
Michael Dela Cruz <michajd1@uci.edu>
2018-10-19 22:41:53 -0700

    pickup update

--- IntershellarGame/Assets/Scripts/ShellPickUp.cs
+++ IntershellarGame/Assets/Scripts/ShellPickUp.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ShellPickUp : MonoBehaviour {
     //Change MoveTest to name of script with GainShell() method
-    public MoveTest shellAdder2;
     public Player_Movement shellAdder;
     public int worth;
 
@@ -24,13 +23,7 @@ public class ShellPickUp : MonoBehaviour {
         if (other.tag == "Player")
         {
             Destroy(gameObject);
-            
-            //Delete this
-            shellAdder2.AddShell(worth);
-            Debug.Log("GotShell");
-
-            //Uncomment when Gain Thing
-            //shellAdder.Gain_Shell(worth);
+            shellAdder.Gain_Shell(worth);
         }
         Debug.Log("Hit");
     }
