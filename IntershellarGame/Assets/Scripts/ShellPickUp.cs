14a6027b17e10f9b672cc9281453002034e5d83f
Michael Dela Cruz <michajd1@uci.edu>
2018-10-19 21:03:50 -0700

    Shell pick up

---
+++ IntershellarGame/Assets/Scripts/ShellPickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellPickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Detects if the player collides with the shell
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log("Hit");
+    }
+}
