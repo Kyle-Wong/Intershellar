ef3eac10903da20a6d7717070e554ab9e207e937
Kyle Wong <kylewonga@gmail.com>
2018-10-21 12:45:33 -0700

    Added persistent music objects

---
+++ IntershellarGame/Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+	// Use this for initialization
+   
+	void Start () {
+        if (GameObject.FindGameObjectsWithTag("UniqueMusicObject").Length > 1)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
