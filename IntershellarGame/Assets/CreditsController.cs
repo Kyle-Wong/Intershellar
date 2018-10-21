ccba0e222355b573cd413aeee05d831a402eca0a
Kyle Wong <kylewonga@gmail.com>
2018-10-20 21:49:10 -0700

    Added credits scene

---
+++ IntershellarGame/Assets/CreditsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreditsController : MonoBehaviour {
+
+    // Use this for initialization
+    public UIRevealer[] names;
+    public UIRevealer[] roles;
+    public float initialDelay;
+    public float repeatDelay;
+
+	void Start () {
+        StartCoroutine(doCredits());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private IEnumerator doCredits()
+    {
+        yield return new WaitForSeconds(initialDelay);
+        for(int i = 0; i < names.Length; i++)
+        {
+            names[i].revealUI();
+            roles[i].revealUI();
+            yield return new WaitForSeconds(repeatDelay);
+        }
+    }
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
