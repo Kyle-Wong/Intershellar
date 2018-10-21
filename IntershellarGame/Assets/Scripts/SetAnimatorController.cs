d8e63e582a889f50b9b0d54b75eb9c1b894433a0
Weiyan Zhu <weiyanz@uci.edu>
2018-10-21 11:12:16 -0700

    Fish Prefab && change Animator controller script

---
+++ IntershellarGame/Assets/Scripts/SetAnimatorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAnimatorController : MonoBehaviour {
+	public RuntimeAnimatorController controller;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator>().runtimeAnimatorController = controller;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
