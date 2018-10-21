64365b1ecc33c09d4b85d152e4b49f47b2c94818
Kyle Wong <kylewonga@gmail.com>
2018-10-20 17:09:43 -0700

    Added orbit ring

---
+++ IntershellarGame/Assets/Scripts/OrbitRing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitRing : MonoBehaviour {
+
+    // Use this for initialization
+    private Planet planet;
+    private float minRange;
+    private float maxRange;
+    public Color farColor;
+    public Color closeColor;
+    private DrawCircle drawer;
+    private Transform player;
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        planet = GetComponent<Planet>();
+        minRange = planet.circleAroundRadius;
+        maxRange = planet.rangeRadius;
+        drawer = GetComponent<DrawCircle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector2.Distance(transform.position,player.position) < maxRange)
+        {
+            float distance = (transform.position - player.position).magnitude;
+            drawer.setRadius(distance);
+            Color c = Color.Lerp(closeColor, farColor, (distance-minRange) / (maxRange-minRange));  //don't set maxrange == minrange fools
+            drawer.setColor(c);
+        }
+
+	}
+}
