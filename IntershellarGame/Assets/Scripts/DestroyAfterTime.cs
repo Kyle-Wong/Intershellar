d0999d751132a2c085786fdf14ca980517f95f42
Weiyan Zhu <weiyanz@uci.edu>
2018-10-20 21:16:25 -0700

    create crab Animator/Animation

--- IntershellarGame/Assets/Scripts/DestroyAfterTime.cs
+++ IntershellarGame/Assets/Scripts/DestroyAfterTime.cs
@@ -6,10 +6,11 @@ public class DestroyAfterTime : MonoBehaviour {
 	public float time;
 	// Use this for initialization
 	void Start () {
+		Destroy(gameObject, time);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Destroy(gameObject, time);
+        
     }
 }
